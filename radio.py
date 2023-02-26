0c679be0bbdfb537f449a209958ff70bfc66f735
John McAleely <john@mcaleely.com>
2023-02-26 15:28:30 +0000

    Add GPIO control for PDN and AON. Document OUT/signal

--- radio.py
+++ radio.py
@@ -1,7 +1,17 @@
 from machine import Pin, Timer
 import time
 
-signal = Pin(0, Pin.IN)
+# PDN: Power Down: High = on ('power down' on)
+# 0 for normal operation
+pdn = Pin(1, mode=Pin.OUT, value=0)
+
+# AON: Auto Gain Control on/off: High = on
+# 1 for normal operation
+aon = Pin(0, mode=Pin.OUT, value=1)
+
+# OUT: Non-inverted output signal
+signal = Pin(2, mode=Pin.IN)
+
 
 pulse_length = 0
 prev_signal = 0
