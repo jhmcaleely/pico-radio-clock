f55f29524744f4378c27994bb4b7319ee693c966
John McAleely <john@mcaleely.com>
2023-04-08 13:44:52 +0100

    General progress

--- radio-plot.rkt
+++ radio-plot.rkt
@@ -3,13 +3,23 @@
 (require csv-reading)
 (require plot)
 
-(define file (open-input-file "Dev\\pico-radio-clock\\data.csv"))
+(define file (open-input-file "data.csv"))
 
-;(define data (csv->list file))
+(define data (csv->list file))
+
+(define start (string->number (second (first data))))
 
 (define (datapoint x)
-  (list (string->number (second x)) (string->number (fourth x))))
+  (let* ((ms (string->number (second x)))
+        (corrected-ms (- ms start))
+        (value (string->number (fourth x))))
+    (list `(,(sub1 corrected-ms) ,(modulo (add1 value) 2)) `(,corrected-ms ,value))))
 
-(define new-data (csv-map datapoint file))
+(define new-data (map datapoint data))
 
-(plot (lines new-data))
+(define (flatten-once lst)
+  (apply append lst))
+
+(plot (lines (flatten-once new-data))
+      #:aspect-ratio (/ 4 1)
+      #:x-label "ticks (ms)")
