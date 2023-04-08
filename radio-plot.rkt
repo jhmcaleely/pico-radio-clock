73e0e84681ef6100aa8e3c94302ef1dc777e84ba
John McAleely <john@mcaleely.com>
2023-04-08 17:12:58 +0100

    some working plots of interrupt and timer based signals

--- radio-plot.rkt
+++ radio-plot.rkt
@@ -9,53 +9,59 @@
 (define signal0 '((0 0.1) (499 0.1) (500 0.9) (999 0.9)))
 (define signaln '((0 0.1) (99 0.1) (100 0.5) (299 0.5) (300 0.9) (999 0.9)))
 
-(define interrupt-file (open-input-file "data.csv"))
-(define timer-file (open-input-file "datatimer.csv"))
+(define (raw-csv-data file)
+  (csv->list (open-input-file file)))
 
-(define interrupt-data (csv->list interrupt-file))
-(define timer-data (csv->list timer-file))
+(define interrupt-data (raw-csv-data "data.csv"))
+(define timer-data (raw-csv-data "datatimer.csv"))
 
 (define (start-offset-from-data lst)
   (+ ref-offset (string->number (second (first lst)))))
 
-(define interrupt-start (start-offset-from-data interrupt-data))
-(define timer-start (start-offset-from-data timer-data))
-
-(define (timer-point-from-row row)
+(define (point-from-row row start)
   (let* ((ms (string->number (second row)))
-         (corrected-ms (- ms timer-start))
+         (corrected-ms (- ms start))
          (value (string->number (fourth row))))
     (list corrected-ms value)))
 
-
-(define (interrupt-point-from-row row)
-  (let* ((ms (string->number (second row)))
-         (corrected-ms (- ms interrupt-start))
-         (value (string->number (fourth row))))
-    (list corrected-ms value)))
-
-(define (interrupt-datapoint row)
-  (let ((point (interrupt-point-from-row row)))
+(define (interrupt-datapoint row start)
+  (let ((point (point-from-row row start)))
     (list `(,(sub1 (first point)) ,(modulo (add1 (second point)) 2)) point)))
 
 (define (flatten-once lst)
   (apply append lst))
 
-(define timer-points (map timer-point-from-row timer-data))
-(define interrupt-points (flatten-once (map interrupt-datapoint interrupt-data)))
+(define timer-points
+  (map (lambda (row) (point-from-row
+                      row
+                      (start-offset-from-data timer-data)))
+       timer-data))
 
-(define (graph start-offset data)
-  (plot (list (lines (map (lambda (n) (list (+ start-offset ref-offset (first n)) (second n))) signal0) #:color 1)
-              (lines (map (lambda (n) (list (+ start-offset ref-offset (first n)) (second n))) signaln) #:color 3)
-              (lines data #:color 2))
+(define interrupt-points
+  (flatten-once (map (lambda (row) (interrupt-datapoint
+                                    row
+                                    (start-offset-from-data interrupt-data)))
+                     interrupt-data)))
+
+(define (offset-points start-offset point)
+   (list (+ start-offset ref-offset (first point)) (second point)))
+
+(define (graph window-offset data)
+  (plot (list (lines (map (lambda (n) (offset-points window-offset n)) signal0) #:color 1)
+              (lines (map (lambda (n) (offset-points window-offset n)) signaln) #:color 2)
+              (lines data #:color 3))
         #:aspect-ratio (/ 4 1)
         #:width 3000
-        #:x-min start-offset
-        #:x-max (+ start-offset plot-window)
+        #:x-min window-offset
+        #:x-max (+ window-offset plot-window)
         #:y-max 1.1
         #:y-min -0.1
         #:x-label "ticks (ms)"))
 
-(map (lambda (n) (graph n timer-points)) (build-list (ceiling (/ 80000 plot-window)) (lambda (x) (* x plot-window))))
+(define plot-windows (build-list (ceiling (/ 80000 plot-window)) (lambda (x) (* x plot-window))))
 
-(map (lambda (n) (graph n interrupt-points)) (build-list (ceiling (/ 80000 plot-window)) (lambda (x) (* x plot-window))))
+(define (all-plots points)
+  (map (lambda (n) (graph n points)) plot-windows))
+
+(all-plots timer-points)
+(all-plots interrupt-points)
